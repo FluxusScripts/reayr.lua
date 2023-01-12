d157655df56c8e8bcfa2afae36fd368709d5acb9
FluxusScripts <118657059+FluxusScripts@users.noreply.github.com>
2023-01-12 21:42:15 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,168 +1,318 @@
+
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/Ag4qAQ5p"))()
+
+
 
 local Window = Library.CreateLib("Руель", "DarkTheme")
 
+
+
 local Tab = Window:NewTab("Скрипты")
+
+
 
 local Section = Tab:NewSection("Просто скрипты")
 
+
+
 Section:NewButton("Флай v3(МНОГО НЕ НАЖИМАТЬ)", "Улетoй)))))", function()
+
+
 
 loadstring(game:HttpGet('https://pastebin.com/raw/YSL3xKYU'))()
 
+
+
     print("Tau#2879")
 
+
+
 end)
+
+
 
 Section:NewButton("Hub Для хауса(Тоже много не использовать)", "ага, думал я еще не убрал описание Buttoninfo?", function()
 
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/aidanlolol/MainScript/main/ChaosScriptVersionTwoPointOne.lua"))()
+
+
 
     print("Tau#2879")
 
+
+
 end)
+
+
 
 Section:NewButton("Старый скрипт chaos", "Старый скрипт на хаус", function()
 
+
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/1111-ssss/Chaos_Script/main/Chaos_script.txt'))()
+
+
 
     print("Tau#2879")
 
+
+
 end)
+
+
 
 Section:NewButton("Старый Флай V2", "? да, мне лень было писать.", function()
 
+
+
 loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
 
-    print("Tau#2879")
 
-end)
-
-Section:NewButton("hoho hub Инфо! --->", "ИГРЫ: ПЕТ СИМ Х,Блокс фрутк и так далее.", function()
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
 
     print("Tau#2879")
 
+
+
 end)
+
+
+
+
+
+
 
 Section:NewButton("Infinite yield", "Infinite Yield, а и так уже понятно", function()
 
+
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+
+
 
     print("Tau#2879") 
 
+
+
 end)
+
+
 
 Section:NewButton("Doors", "Даёт скипт на дорс", function()
 
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Doors"))()
+
+
 
     print("Tau#2879")
 
+
+
 end)
+
+
 
 Section:NewButton("Domainx(Закрывает предметы)", "Domainx", function()
 
+
+
 loadstring(game:HttpGet('https://pastebin.com/raw/YSL3xKYU'))()
+
+
 
     print("Tau#2879")
 
+
+
 end)
+
+
 
 local Tab = Window:NewTab("Функции")
 
+
+
 local Section = Tab:NewSection("Функции")
+
+
 
 Section:NewSlider("Бегать", "Быстро бегать", 500, 0, function(s)
 
+
+
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+
+
 
 end)
 
+
+
 Section:NewTextBox("Бег", "TextboxInfo", function(txt)
+
+
 
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = txt
 
+
+
 	print(txt)end)
+
 
 Section:NewSlider("Прыжок", ".", 500, 0, function(s) 
 
+
+
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+
+
 
 end)
 
 Section:NewTextBox("Прыжок", "?", function(txt)
 
+
+
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = txt
+
+
 
 	print(txt)
 
+
+
 end)
+
+
 
 Section:NewSlider("жизнь(не показывает игрокам)", "SliderInfo, забыл убрать.уфф", 100, 0, function(s) 
 
+
+
     game.Players.LocalPlayer.Character.Humanoid.Health = s
 
+
+
 end)
+
+
 
 local Tab = Window:NewTab("Кредиты")
 
+
+
 local Section = Tab:NewSection("Ютуб: Любитель Arceus x")
+
+
 
 Section:NewButton("Дс: Tau#2879 ЧЕК ИНФО", "Не отвечаю напиши в лс СЕРВЕР и я тебе его скину", function()
 
+
+
     print("Tau#2879")
+
+
 
 end)
 
-Section:NewKeybind("Закрывания хаба на клавишу..", "Клавиша закрывания хаба", Enum.KeyCode.F, function()
+
+
+Section:NewKeybind("Закрывания хаба на клавишу..", "Клавиша закрывания хаба", Enum.KeyCode.F6, function()
+
+
 
 	Library:ToggleUI()
 
+
+
 end)
+
+
 
 Section:NewButton("Клавиатура(Для телефонов)", "Даёт кклав коку", function()
 
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+
+
 
     print("Tau#2879")
 
+
+
 end)
+
+
 
 local Tab = Window:NewTab("Обновы.       Смс +5!")
 
+
+
 local Section = Tab:NewSection("0 Обнова. вышел хаб.")
 
+
+
 local Section = Tab:NewSection("1. Обнова. добавление новых кнопок")
+
+
 
 local Section = Tab:NewSection("2.Вышел новый хаб. Можно двигать это окно")
 
 local Section = Tab:NewSection("3.Обновление темы.")
 
+
+
 local Section = Tab:NewSection("4.Новый таб Функции.")
+
+
 
 local Section = Tab:NewSection("5.Новые кнопки в (Больше скриптов) и (FE)")
 
+
+
 local Tab = Window:NewTab("Fe")
+
+
 
 local Section = Tab:NewSection("Fe")
 
+
+
 Section:NewButton("Крипер оу нет", "Кредиты для I'm Patrick", function()
+
+
 
 loadstring(game:HttpGet(('https://pastefy.ga/Kqi7ZcbX/raw'),true))()
 
+
+
     print("Tau#2879")
 
+
+
 end)
+
+
 
 Section:NewButton("меню с FE скриптами","Кредиты для DVRKNVSS", function()
 
+
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Dvrknvss/UniversalFEScriptHub/main/Script'))()
+
+
 
 print("Tau#2879")
 
+
+
 end)
+
+
 
 local Tab= Window:NewTab("Чтобы спамить в чат")
 
@@ -170,9 +320,13 @@ local Section = Tab:NewSection("СПАМ")
 
 Section:NewButton("Жми!", "Кредиты мне.", function()
 
+
+
 -- Script generated by SimpleSpy - credits to exx#9394
 
 local args = {
+
+
 
     [1] = "REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE REAYR YOUTUBE ",
 
@@ -186,6 +340,8 @@ game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageReque
 
 end)
 
+
+
 local Tab = Window:NewTab("Больше скриптов!")
 
 local Section = Tab:NewSection("Авто фарм")
@@ -198,13 +354,19 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/BloxiYT/Diamond/main/
 
 end)
 
+
+
 Section:NewButton("Arsenal!","Кредиты для ?", function()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Arsenal"))()
 
 print("Tau#2879")
 
+
+
 end)
+
+
 
 Section:NewButton("Evade","Кредиты для ?", function()
 
@@ -214,6 +376,8 @@ print("Tau#2879")
 
 end)
 
+
+
 Section:NewButton("Storng man!","Кредиты для ?", function()
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/hngamingroblox/scripts/main/strongman%20simulator'))()
@@ -221,6 +385,8 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/hngamingroblox/script
 print("Tau#2879")
 
 end)
+
+
 
 Section:NewButton("Break in","Кредиты для ?", function()
 
@@ -238,59 +404,280 @@ print("Tau#2879")
 
 end)
 
+
+
+Section:NewButton("KAT","Кредиты для ?", function()
+
+loadstring(game:HttpGet("https://pastebin.com/raw/hkvHeHed",true))()
+
+print("Tau#2879")
+
+
+
+end)
+
+Section:NewButton("VHub Инфо-->","Ключ KillYourself", function()
+
+
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Veincx5315/Created/VHub/Simple'),true))()
+
+
+
+print("Tau#2879")
+
+
+
+end)
+
 Section:NewButton("Mini hub с скриптами для игр","Кредиты для ?", function()
 
+
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MiniNoobie/MINI-HUB-V2/main/FINALLY%20UPDATED%20MINI%20HUB",true))()
+
+
+
+print("Tau#2879")
+
+
+
+end)
+
+Section:NewButton("Field Goal Simulator 100% точность","Кредиты для ?", function()
+
+local mt = getrawmetatable(game)
+
+local old = mt.__namecall
+
+setreadonly(mt, false)
+
+mt.__namecall =
+
+   newcclosure(
+
+   function(remote, ...)
+
+       args = {...}
+
+       method = tostring(getnamecallmethod())
+
+       if method == "FireServer" and tostring(remote) == "ServerEvent" and args[1] == "Accuracy" then
+
+           args[2] = 1
+
+           return old(remote, unpack(args))
+
+       end
+
+       return old(remote, ...)
+
+   end
+
+)
+
+setreadonly(mt, true)
 
 print("Tau#2879")
 
 end)
-Section:NewButton("Mini hub с скриптами для игр","Кредиты для ?", function()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MiniNoobie/MINI-HUB-V2/main/FINALLY%20UPDATED%20MINI%20HUB",true))()
 
-print("Tau#2879")
 
-end)
+
+
+
 
 local Tab = Window:NewTab("Больше..")
 
+
+
 local Section = Tab:NewSection("Просим прощения но дальше нельзя")
+
+
 
 local Section = Tab:NewSection("Мы сделаем раздельно этот хаб.")
 
+
+
 local Section = Tab:NewSection("Если я буду делать этот хаб долго")
+
+
 
 local Section = Tab:NewSection("То напишите мне в лс дс чтобы я делал быстрее")
 
+
+
 local Tab = Window:NewTab("Подрежка!")
+
+
 
 local Section = Tab:NewSection("Писать Tau#2879")
 
+
+
 local Section = Tab:NewSection("А если что то важное пишите мне в лс ~Сервер~")
 
-local Section = Tab:NewSection("200 линей хаба(Gui)")
+
+
+
 
 game.StarterGui:SetCore("SendNotification", { 
+
+
 
 Title = "Руель"; 
 
+
+
 Text = "Хъх"; -- what the text says (ofc) 
 
+
+
 Duration = 30; 
+
+
 
 }) 
 
+
+
 wait(1) 
+
+
 
 game.StarterGui:SetCore("SendNotification", { 
 
+
+
 Title = "Хъх"; 
+
+
 
 Text = "От меня(Не красть)"; 
 
+
+
 Duration = 30; 
+
+
 
 })
 
+
+
+local ScreenGui = Instance.new("ScreenGui")
+
+local Frame = Instance.new("Frame")
+
+local TextButton = Instance.new("TextButton")
+
+local Frame2 = Instance.new("Frame")
+
+local TextButton2 = Instance.new("TextButton")
+
+
+
+ScreenGui.Parent = game.CoreGui
+
+
+
+Frame.Parent = ScreenGui
+
+Frame.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
+
+Frame.BackgroundTransparency = 1
+
+Frame.Position = UDim2.new(0, 0, 0.449392706, 0)
+
+Frame.Size = UDim2.new(0, 90, 0, 40)
+
+Frame.Active = true
+
+Frame.Draggable = true
+
+
+
+TextButton.Parent = Frame
+
+TextButton.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
+
+TextButton.BackgroundTransparency = 0.500
+
+TextButton.Size = UDim2.new(0, 90, 0, 40)
+
+TextButton.Font = Enum.Font.SourceSans
+
+TextButton.Text = "Закрыть Руель"
+
+TextButton.TextColor3 = Color3.fromRGB(254, 254, 254)
+
+TextButton.TextSize = 16.000
+
+TextButton.MouseButton1Click:connect(function()
+
+Frame.Visible = false
+
+Frame2.Visible = true    
+
+local vim = game:service("VirtualInputManager")
+
+vim:SendKeyEvent(true, "F6", false, game)
+
+end)
+
+
+
+Frame2.Parent = ScreenGui
+
+Frame2.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
+
+Frame2.BackgroundTransparency = 0.500
+
+Frame2.Position = UDim2.new(0, 0, 0.449392706, 0)
+
+Frame2.Size = UDim2.new(0, 68, 0, 25)
+
+Frame2.Active = true
+
+Frame2.Draggable = true
+
+
+
+TextButton2.Parent = Frame2
+
+TextButton2.BackgroundColor3 = Color3.fromRGB(71, 71, 71)
+
+TextButton2.BackgroundTransparency = 0.500
+
+TextButton2.Size = UDim2.new(0, 90, 0, 40)
+
+TextButton2.Font = Enum.Font.SourceSans
+
+TextButton2.Text = "Закрыть Руель"
+
+TextButton2.TextColor3 = Color3.fromRGB(254, 254, 254)
+
+TextButton2.TextSize = 17.000
+
+TextButton2.MouseButton1Click:connect(function()
+
+    Frame2.Visible = false
+
+    Frame.Visible = true
+
+local vim = game:service("VirtualInputManager")
+
+vim:SendKeyEvent(true, "F6", false, game)
+
+end)
+
+
+
+Frame.Parent.Active = true
+
+Frame.Parent.Draggable = true
+
  
+
+
